eb9863120ec976a3bae8fb3b4152f9715c20b64e
20CNT <vncnt.p.98@gmail.com>
2025-06-06 08:37:42 +0200

    [ADD]: Table components

---
+++ frontend/src/app/components/cell/cell.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cell',
+  imports: [],
+  templateUrl: './cell.component.html',
+  styleUrl: './cell.component.css'
+})
+export class CellComponent {
+
+}
