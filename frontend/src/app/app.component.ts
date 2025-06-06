81783e01b387fcfe8be9a96a66ebd6b301308cc7
Vincent Philippe <viphilip@z1r9p4.42lyon.fr>
2025-06-06 14:43:18 +0200

    [FIX] http

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 import { TableComponent } from './components/table/table.component';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     TableComponent,
-    HttpClientModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
